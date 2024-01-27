b0ad9eb38c623f93e4830bb45f92bb0f149f7567
Karan <risingphoenix160@gmail.com>
2024-01-27 23:59:00 +0530

    refactor: minor changes

--- js/script.js
+++ js/script.js
@@ -1,48 +1,56 @@
 (function () {
-  const $cursor = document.getElementById('cursor');
-  const $videoCursor = document.getElementById('video-cursor');
-  const $testmonialReview = document.getElementById('review');
+  const $cursor = document.getElementById("cursor");
+  const $videoCursor = document.getElementById("video-cursor");
+  const $testmonialReview = document.getElementById("review");
 
   const testimonials = [
     {
-      id: 'm// 001',
-      name: 'Cartier',
-      review: 'Thank you so much for the beautiful catering; it means a lot working with a company such as two good co.'
+      id: "m// 001",
+      name: "Cartier",
+      review:
+        "Thank you so much for the beautiful catering; it means a lot working with a company such as two good co.",
     },
     {
-      id: 'm// 002',
-      name: 'Felicity T',
-      review: 'The hampers we ordered were lovely and the team are wonderful to liase with.'
+      id: "m// 002",
+      name: "Felicity T",
+      review:
+        "The hampers we ordered were lovely and the team are wonderful to liase with.",
     },
     {
-      id: 'm// 003',
-      name: 'Barbara',
-      review: 'My package just arrived and the presentation is so beautiful; elegant, magical and meaningful, with the items wrapped in delicious-smelling tissue paper. Georgeous; Will be back for more.'
+      id: "m// 003",
+      name: "Barbara",
+      review:
+        "My package just arrived and the presentation is so beautiful; elegant, magical and meaningful, with the items wrapped in delicious-smelling tissue paper. Georgeous; Will be back for more.",
     },
     {
-      id: 'm// 004',
-      name: 'Salesforce',
-      review: 'I think I speak for everyone when I say we are very grateful to have been able to come in and help out for the day; The work you do is amazing.'
+      id: "m// 004",
+      name: "Salesforce",
+      review:
+        "I think I speak for everyone when I say we are very grateful to have been able to come in and help out for the day; The work you do is amazing.",
     },
     {
-      id: 'm// 005',
-      name: 'Cartier',
-      review: 'Thank you so much for the beautiful catering; it means a lot working with a company such as two good co.'
+      id: "m// 005",
+      name: "Cartier",
+      review:
+        "Thank you so much for the beautiful catering; it means a lot working with a company such as two good co.",
     },
     {
-      id: 'm// 006',
-      name: 'Felicity T',
-      review: 'The hampers we ordered were lovely and the team are wonderful to liase with.'
+      id: "m// 006",
+      name: "Felicity T",
+      review:
+        "The hampers we ordered were lovely and the team are wonderful to liase with.",
     },
     {
-      id: 'm// 007',
-      name: 'Barbara',
-      review: 'My package just arrived and the presentation is so beautiful; elegant, magical and meaningful, with the items wrapped in delicious-smelling tissue paper. Georgeous; Will be back for more.'
+      id: "m// 007",
+      name: "Barbara",
+      review:
+        "My package just arrived and the presentation is so beautiful; elegant, magical and meaningful, with the items wrapped in delicious-smelling tissue paper. Georgeous; Will be back for more.",
     },
     {
-      id: 'm// 008',
-      name: 'Salesforce',
-      review: 'I think I speak for everyone when I say we are very grateful to have been able to come in and help out for the day; The work you do is amazing.'
+      id: "m// 008",
+      name: "Salesforce",
+      review:
+        "I think I speak for everyone when I say we are very grateful to have been able to come in and help out for the day; The work you do is amazing.",
     },
   ];
 
@@ -50,61 +58,84 @@
 
   const locoScroll = new LocomotiveScroll({
     el: document.querySelector("[data-scroll-container]"),
-    smooth: true
+    smooth: true,
   });
   locoScroll.on("scroll", ScrollTrigger.update);
 
   ScrollTrigger.scrollerProxy("[data-scroll-container]", {
     scrollTop(value) {
-      return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
     },
     getBoundingClientRect() {
-      return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
     },
-    pinType: document.querySelector("[data-scroll-container]").style.transform ? "transform" : "fixed"
+    pinType: document.querySelector("[data-scroll-container]").style.transform
+      ? "transform"
+      : "fixed",
   });
 
   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
   ScrollTrigger.config({
-    autoRefreshEvents: "DOMContentLoaded,load,visibilitychange" // omit resize
+    autoRefreshEvents: "DOMContentLoaded,load,visibilitychange", // omit resize
   });
 
   ScrollTrigger.refresh();
 
+  new Swiper(".testimonial__authors", {
+    slidesPerView: "auto",
+    spaceBetween: 50,
+    loop: true,
+    // speed: 1000,
+    grabCursor: true,
+    mousewheelControl: true,
+    keyboardControl: true,
+    // autoplay: {
+    //   delay: 1,
+    //   disableOnInteraction: false,
+    // },
+  });
+
   class PreLoader {
-    constructor ($elem) {
+    constructor($elem) {
       this.$elem = $elem;
       this.tl = null;
     }
 
     start() {
-      this.tl = gsap.to(this.$elem + ' > div', {
+      this.tl = gsap.to(this.$elem + " > div", {
         rotate: 360,
-        repeat: -1
+        repeat: -1,
       });
     }
 
     stop() {
       gsap.to(this.$elem, {
-        y: '-100%',
-        duration: 0.3
+        y: "-100%",
+        duration: 0.3,
       });
 
       this.tl.pause();
     }
-  };
+  }
 
   class VideoPlayer {
-    constructor (elem) {
+    constructor(elem) {
       this.$player = document.querySelector(elem);
-      this.$video = this.$player.querySelector('video');
+      this.$video = this.$player.querySelector("video");
 
       this.addEventListeners();
     }
 
     addEventListeners() {
-      this.$player.addEventListener('click', () => this.toggleFullScreen());
+      this.$player.addEventListener("click", () => this.toggleFullScreen());
 
       this.$video.play(); // For now, later will do something different
     }
@@ -115,35 +146,37 @@
       } else {
         document.exitFullscreen();
       }
-      this.$player.classList.toggle('fullscreen');
+      this.$player.classList.toggle("fullscreen");
       this.$video.muted = !this.$video.muted;
     }
-  };
+  }
 
-  const preLoader = new PreLoader('#preloader');
+  const preLoader = new PreLoader("#preloader");
 
   preLoader.start();
 
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     preLoader.stop();
 
-    new VideoPlayer('.hero__video');
+    new VideoPlayer(".hero__video");
 
-    gsap.to('.hero__title > span > span', {
+    gsap.to(".hero__title > span > span", {
       y: 0,
       opacity: 1,
       stagger: 0.2,
-      delay: 0.3
+      delay: 0.3,
     });
   });
 
   function loadTestimonials() {
-    let clutter = '';
+    let clutter = "";
 
     testimonials.forEach((testimonial, i) => {
       clutter += `
-        <div class="testimonial__author">
-          <input type="radio" name="author" id="${testimonial.id}" ${i == 0 ? 'checked' : ''}>
+        <div class="testimonial__author swiper-slide">
+          <input type="radio" name="author" id="${testimonial.id}" ${
+        i == 0 ? "checked" : ""
+      }>
           <label for="${testimonial.id}">
             <span class="visually-hidden">${testimonial.name}'s Review</span>
           </label>
@@ -155,18 +188,23 @@
       `;
     });
 
-    document.querySelector('.testimonial__authors').innerHTML = clutter;
-  };
+    document.querySelector(".testimonial__authors .swiper-wrapper").innerHTML =
+      clutter;
+  }
 
   loadTestimonials();
 
-  document.addEventListener('click', (event) => {
-    if (event.target.closest('.menu-toggle')) {
-      toggleMenu(event.target.closest('.menu-toggle'));
-    } else if (event.target.closest('.testimonial__author > input')) {
-      const testimonialId = event.target.closest('.testimonial__author > input').id;
+  document.addEventListener("click", (event) => {
+    if (event.target.closest(".menu-toggle")) {
+      toggleMenu(event.target.closest(".menu-toggle"));
+    } else if (event.target.closest(".testimonial__author > input")) {
+      const testimonialId = event.target.closest(
+        ".testimonial__author > input"
+      ).id;
 
-      const review = testimonials.find(testimonial => testimonial.id === testimonialId)?.review;
+      const review = testimonials.find(
+        (testimonial) => testimonial.id === testimonialId
+      )?.review;
 
       if (review) {
         $testmonialReview.textContent = review;
@@ -175,137 +213,145 @@
   });
 
   function toggleMenu($menuToggle) {
-    const expanded = $menuToggle.getAttribute('aria-expanded');
+    const expanded = $menuToggle.getAttribute("aria-expanded");
     const $menu = document.getElementById(
-      $menuToggle.getAttribute('aria-controls')
+      $menuToggle.getAttribute("aria-controls")
     );
 
-    if (expanded === 'true') {
+    if (expanded === "true") {
       gsap.to($menu, {
-        y: '-100%',
-        duration: 0.3
+        y: "-100%",
+        duration: 0.3,
       });
-      gsap.to('.header__logo', {
-        color: 'black',
-        duration: 0.3
+      gsap.to(".header__logo", {
+        color: "black",
+        duration: 0.3,
       });
-      $menuToggle.setAttribute('aria-expanded', false);
+      $menuToggle.setAttribute("aria-expanded", false);
     } else {
       gsap.to($menu, {
         y: 0,
-        duration: 0.3
+        duration: 0.3,
       });
-      gsap.to('.header__logo', {
-        color: 'white',
-        duration: 0.3
+      gsap.to(".header__logo", {
+        color: "white",
+        duration: 0.3,
       });
-      $menuToggle.setAttribute('aria-expanded', true);
+      $menuToggle.setAttribute("aria-expanded", true);
     }
-    $menuToggle.classList.toggle('expanded');
+    $menuToggle.classList.toggle("expanded");
   }
 
   function headerScrollAction($elem, props) {
     gsap.to($elem, {
       scrollTrigger: {
-        trigger: '.header',
-        scroller: '[data-scroll-container]',
+        trigger: ".header",
+        scroller: "[data-scroll-container]",
         start: "top 0",
         end: "top -15%",
-        scrub: true
+        scrub: true,
       },
-      ...props
+      ...props,
     });
   }
 
-  headerScrollAction('#secondary-nav', { y: '-100%', opacity: 0 });
+  headerScrollAction("#secondary-nav", { y: "-100%", opacity: 0 });
 
-  headerScrollAction('.default-icon, .scrolled-icon', { y: -83 });
+  headerScrollAction(".default-icon, .scrolled-icon", { y: -83 });
 
   function followMouse(event, $cursor) {
-    $cursor.style.top = event.y + 'px';
-    $cursor.style.left = event.x + 'px';
+    $cursor.style.top = event.y + "px";
+    $cursor.style.left = event.x + "px";
     gsap.to($cursor, {
-      x: '-50%',
-      y: '-50%',
+      x: "-50%",
+      y: "-50%",
       top: event.y,
       left: event.x,
       scale: 1,
-      opacity: 1
+      opacity: 1,
     });
   }
 
-  if (window.matchMedia('(pointer: fine)').matches) {
-    document.addEventListener('mousemove', (event) => {
-      if (event.target.closest('.hero__video')) {
+  if (window.matchMedia("(pointer: fine)").matches) {
+    document.addEventListener("mousemove", (event) => {
+      if (event.target.closest(".hero__video")) {
         followMouse(event, $videoCursor);
-      }
-      else if (event.target.closest('.products__card')) {
-        const target = event.target.closest('.products__card');
+      } else if (event.target.closest(".products__card")) {
+        const target = event.target.closest(".products__card");
         gsap.to($cursor, {
-          backgroundColor: `rgba(${target.getAttribute('data-rgba')})`
+          backgroundColor: `rgba(${target.getAttribute("data-rgba")})`,
         });
         followMouse(event, $cursor);
-      }
-      else {
+      } else {
         gsap.to($cursor, {
           scale: 0,
-          opacity: 0
+          opacity: 0,
         });
         gsap.to($videoCursor, {
           scale: 0,
-          opacity: 0
+          opacity: 0,
         });
       }
     });
   }
 
-  const sectionTriggerProps = $trigger => ({
+  const sectionTriggerProps = ($trigger) => ({
     trigger: $trigger,
-    scroller: '[data-scroll-container]',
+    scroller: "[data-scroll-container]",
     start: "top bottom",
-    end: window.matchMedia('(min-width: 50em)').matches ? "bottom 105%" : "bottom 120%",
-    scrub: true
+    end: () =>
+      window.matchMedia("(min-width: 50em)").matches
+        ? "bottom 105%"
+        : "bottom 120%",
+    scrub: true,
   });
 
   function cardTransition($elem, $trigger) {
     gsap.from($elem, {
       scrollTrigger: sectionTriggerProps($trigger),
-      y: '10vh',
+      y: "10vh",
       opacity: 0,
-      stagger: 0.2
+      stagger: 0.2,
     });
   }
 
-  cardTransition('.products__card', '.products');
+  [
+    [".products__card", ".products"],
+    [".featured__item", ".featured"],
+    [".footer__item svg > path", ".footer__wrapper"],
+    [".impact .scroll-img-wrapper", ".impact"],
+  ].forEach(([elem, target]) => cardTransition(elem, target));
 
-  cardTransition('.featured__item', '.featured');
-
-  cardTransition('.footer__item svg > path', '.footer__wrapper');
-
-  cardTransition('.impact .scroll-img-wrapper', '.impact');
+  // cardTransition(".products__card", ".products");
+  // cardTransition(".featured__item", ".featured");
+  // cardTransition(".footer__item svg > path", ".footer__wrapper");
+  // cardTransition(".impact .scroll-img-wrapper", ".impact");
 
   function headingTransition($trigger) {
-    gsap.to('[data-scroll-heading]', {
+    gsap.to("[data-scroll-heading]", {
       scrollTrigger: sectionTriggerProps($trigger),
       y: 0,
-      opacity: 1
+      opacity: 1,
     });
   }
 
-  ['.belief', '.testimonial'].forEach(item => headingTransition(item));
+  [".belief", ".testimonial"].forEach((item) => headingTransition(item));
 
   function sectionTitleTransition($trigger) {
-    gsap.to($trigger + ' .section__title', {
+    gsap.to($trigger + " .section__title", {
       scrollTrigger: {
         ...sectionTriggerProps($trigger),
         start: "top bottom",
-        end: "20% 80%"
+        end: "20% 80%",
       },
-      '--_underline-scale': '1'
+      "--_underline-scale": "1",
     });
   }
 
-  ['.featured__wrapper', '.testimonial__wrapper', '.footer'].forEach($section => sectionTitleTransition($section));
+  [".featured__wrapper", ".testimonial__wrapper", ".footer"].forEach(
+    ($section) => sectionTitleTransition($section)
+  );
 
-  document.getElementById('copyright-year').textContent = new Date().getFullYear();
+  document.getElementById("copyright-year").textContent =
+    new Date().getFullYear();
 })();
